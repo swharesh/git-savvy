fda23eb97c7ec8d95dacc1e8297937562ca58d93
swharesh <swharesh@deloitte.com>
2024-03-11 19:07:32 +0530

    llol ai

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import rich
+rich.print("Hello, [bold green]Geeks[/bold green]!")
+dic={ 'lol':1243234,
+    "dsf":"sdfsdf",
+    "cgdfg":{"fdsg":"dfg","hh":3432}
+}
+a=10
+lol="boi"
+rich.print(dic)
